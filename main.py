7ba37e3aabe0e6f1b90dfb5f83d9fadbc835fcdb
syaidi18 <56240679+syaidi18@users.noreply.github.com>
2020-01-13 09:38:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-class MainApp():
-    def __init__(self):
+from core.baseapp import BaseApp
+from core.search_helper import SearchHelper
+from data_model.author import Author
+from data_model.book import Book
+from data_model.publication import Publication
+
+class MainApp(BaseApp):
+    def _init_(self):
         self.books = []
+        self.InputBook = []
+        self.ViewBook = []
+        BaseApp._init_(self)
 
 
-if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+class ViewBook(Book):
+    def _init_(self):
+        vBook = ViewBook (self, books)
+        vBook.list_book()
 
+
+    if _name_ == "_main_":
+        app = MainApp(BaseApp)
+        app.run()
+
+
+    @property
+    def list_book(self):
+        return self.list_book
+    def add_book(self):
+        return self.add_book()
+    def search_book(self):
+        return self.search_book()
